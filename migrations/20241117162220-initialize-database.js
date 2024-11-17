af8a9efdc4004eb6e62c492537c7211c93477e98
Petri Kotiranta <petri.kotiranta@exaum.com>
2024-11-17 18:58:21 +0200

    13.17

---
+++ migrations/20241117162220-initialize-database.js
@@ -0,0 +1,68 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('notes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      important: {
+        type: DataTypes.BOOLEAN,
+      },
+      date: {
+        type: DataTypes.DATE,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+    await queryInterface.addColumn('notes', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('notes');
+    await queryInterface.dropTable('users');
+  },
+};
